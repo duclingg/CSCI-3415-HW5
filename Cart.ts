9307bfd11614d0489e0130d815ac266161475cef
Justin Hoang <justin.lhoang3@gmail.com>
2025-04-27 01:20:48 -0600

    move object creation to app.ts

--- Cart.ts
+++ Cart.ts
@@ -1,7 +1,7 @@
 import * as Type from "./Types";
 import * as Product from "./Products"
 
-class Cart {
+export class Cart {
     private MAX_ITEMS: number = 7;
     private itemNum: number = 0;
     private owner: Type.NameType;
@@ -37,7 +37,7 @@ class Cart {
     }
     
     public addItem(product: Product.Product): boolean {
-        if (this.itemNum < this.MAX_ITEMS) {
+        if (!this.isCartFull()) {
             this.purchasedItems.push(product);
             this.itemNum++;
             return true;
@@ -54,6 +54,7 @@ class Cart {
                 return true;
             }
         }
+        console.log("There are no items in the cart!");
         return false;
     }
 
@@ -65,56 +66,10 @@ class Cart {
     }
 
     private isCartFull(): boolean {
-        if (this.itemNum == this.MAX_ITEMS) {
+        if (this.itemNum === this.MAX_ITEMS) {
             return true;
         } else {
             return false;
         }
     }
 }
-
-const owner: Type.NameType = { firstName: "Justin", lastName: "Hoang" };
-const cart = new Cart(owner);
-
-const song1 = new Product.AudioProduct("Outta Time", 49.99, { firstName: "Bryson", lastName: "Tiller" });
-song1.setGenre(Type.GenreType.RnB);
-song1.setReviewRate(8.5);
-
-const song2 = new Product.AudioProduct("Sweeter", 39.99, { firstName: "Leon", lastName: "Bridges" });
-song2.setGenre(Type.GenreType.Blues);
-song2.setReviewRate(9.0);
-
-const song3 = new Product.AudioProduct("Work", 39.99, { firstName: "Charlotte Day", lastName: "Wilson" });
-song3.setGenre(Type.GenreType.RnB);
-song3.setReviewRate(9.3);
-
-const song4 = new Product.AudioProduct("I Want You Around", 39.99, { firstName: "Snoh", lastName: "Algera" });
-song4.setGenre(Type.GenreType.RnB);
-song4.setReviewRate(10.0);
-
-const movie1 = new Product.VideoProduct("The Pursuit of Happiness", 9.99, { firstName: "Gabriele", lastName: "Muccino"}, 2006, 117);
-movie1.setFilmRate(Type.FilmRateType.PG);
-movie1.setReviewRate(9.1);
-
-const movie2 = new Product.VideoProduct("My Neighbor Totoro", 9.99, { firstName: "Hayao", lastName: "Miyazaki" }, 1988, 86);
-movie2.setFilmRate(Type.FilmRateType.PG);
-movie2.setReviewRate(10.0);
-
-const book1 = new Product.PaperBookProduct("The Creative Act", 19.99, { firstName: "Rick", lastName: "Rubin" }, 250);
-book1.setReviewRate(7.9);
-
-const book2 = new Product.EBookProduct("Sapiens: A Brief History of Mankind", 23.99, { firstName: "Yuval Noah", lastName: "Harari" }, 200);
-book2.setReviewRate(8.2);
-
-cart.addItem(song1);
-cart.addItem(song2);
-cart.addItem(song3);
-cart.addItem(movie1);
-cart.addItem(movie2);
-cart.addItem(book1);
-cart.addItem(book2);
-
-// cart.addItem(song4);
-
-cart.displayCart();
-console.log(cart.getItemNum());
---
+++ app.ts
@@ -0,0 +1,41 @@
+import { Cart } from './Cart';
+import * as Type from './Types';
+import * as Product from './Products';
+
+const owner: Type.NameType = { firstName: "Justin", lastName: "Hoang" };
+const cart = new Cart(owner);
+
+const song1 = new Product.AudioProduct("Outta Time", 49.99, { firstName: "Bryson", lastName: "Tiller" });
+song1.setGenre(Type.GenreType.RnB);
+song1.setReviewRate(8.5);
+
+const song2 = new Product.AudioProduct("Sweeter", 39.99, { firstName: "Leon", lastName: "Bridges" });
+song2.setGenre(Type.GenreType.Blues);
+song2.setReviewRate(9.0);
+
+const song3 = new Product.AudioProduct("Work", 39.99, { firstName: "Charlotte Day", lastName: "Wilson" });
+song3.setGenre(Type.GenreType.RnB);
+song3.setReviewRate(9.3);
+
+const song4 = new Product.AudioProduct("I Want You Around", 39.99, { firstName: "Snoh", lastName: "Algera" });
+song4.setGenre(Type.GenreType.RnB);
+song4.setReviewRate(10.0);
+
+const movie1 = new Product.VideoProduct("The Pursuit of Happiness", 9.99, { firstName: "Gabriele", lastName: "Muccino"}, 2006, 117);
+movie1.setFilmRate(Type.FilmRateType.PG);
+movie1.setReviewRate(9.1);
+
+const movie2 = new Product.VideoProduct("My Neighbor Totoro", 9.99, { firstName: "Hayao", lastName: "Miyazaki" }, 1988, 86);
+movie2.setFilmRate(Type.FilmRateType.PG);
+movie2.setReviewRate(10.0);
+
+const book1 = new Product.PaperBookProduct("The Creative Act", 19.99, { firstName: "Rick", lastName: "Rubin" }, 250);
+book1.setReviewRate(7.9);
+
+const book2 = new Product.EBookProduct("Sapiens: A Brief History of Mankind", 23.99, { firstName: "Yuval Noah", lastName: "Harari" }, 200);
+book2.setReviewRate(8.2);
+
+cart.removeItem(song1.productID);
+
+cart.displayCart();
+console.log(cart.getItemNum());
